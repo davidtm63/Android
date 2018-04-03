3c5ee734ce3d9e14bb6f92ce1943e377c4b37ee9
David <davidtm63@gmail.com>
2018-04-03 17:04:58 +0200

    a la mierda
    juarls

--- MyMapTest/app/src/main/java/com/example/usuari/mymaptest/MapsActivity.java
+++ MyMapTest/app/src/main/java/com/example/usuari/mymaptest/MapsActivity.java
@@ -40,7 +40,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("A la mierda "));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));    
     }
 }
